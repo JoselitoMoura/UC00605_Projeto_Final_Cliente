04f71b6dc2b3889d24a000a4e233e098e49bdf79
JoselitoMoura <joselitodemourabarbosa>
2026-06-13 00:21:30 +0100

    Criação das dependências no arquivo pom.xml

--- src/main/java/pt/cadastro_cliente/CadastroClienteApplication.java
+++ src/main/java/pt/cadastro_cliente/CadastroClienteApplication.java
@@ -1,13 +1,13 @@
-package pt.uc00605_projeto_final_cliente;
+package pt.cadastro_cliente;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProjetoFinalClienteApplication {
+public class CadastroClienteApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProjetoFinalClienteApplication.class, args);
+		SpringApplication.run(CadastroClienteApplication.class, args);
 	}
 
 }
---
+++ src/test/java/pt/cadastro_cliente/CadastroClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.cadastro_cliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroClienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
