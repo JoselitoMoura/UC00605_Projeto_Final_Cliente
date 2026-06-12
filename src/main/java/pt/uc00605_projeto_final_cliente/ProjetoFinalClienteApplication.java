6f7da530f81cf41ce78822efae36e11da03cb073
JoselitoMoura <joselitodemourabarbosa>
2026-06-12 23:23:08 +0100

    Atualização do arquivo pom.xml com a inserção das dependencias.

---
+++ src/main/java/pt/uc00605_projeto_final_cliente/ProjetoFinalClienteApplication.java
@@ -0,0 +1,13 @@
+package pt.uc00605_projeto_final_cliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoFinalClienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoFinalClienteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pt/uc00605_projeto_final_cliente/ProjetoFinalClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.uc00605_projeto_final_cliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoFinalClienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
