4535c8b4b56ee518490bee4c07c79df56c8a3f93
JoselitoMoura <joselitodemourabarbosa>
2026-06-13 09:47:12 +0100

    Ajuste no comentário

--- src/main/java/pt/cadastro_cliente/CadastroClienteApplication.java
+++ src/main/java/pt/cadastro_cliente/CadastroClienteApplication.java
@@ -1,3 +1,6 @@
+/*
+ * Arquivo que inicializa o programa de Cadastro de Cliente.
+*/
 package pt.cadastro_cliente;
 
 import org.springframework.boot.SpringApplication;
@@ -5,7 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CadastroClienteApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CadastroClienteApplication.class, args);
 	}
