8964d667ab6b7217ea5658ec342c0373ce60097b
JoselitoMoura <joselitodemourabarbosa>
2026-06-13 09:32:21 +0100

    Ajustes nos comentários do arquivo.

--- src/main/java/pt/cadastro_cliente/controller/ClienteController.java
+++ src/main/java/pt/cadastro_cliente/controller/ClienteController.java
@@ -1,16 +1,24 @@
 package pt.cadastro_cliente.controller;
 
-import pt.cadastro_cliente.model.Cliente;
-import pt.cadastro_cliente.service.ClienteService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 import java.util.Optional;
 
-/**
- * Controller recebe as requisições HTTP (API REST).
- */
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import pt.cadastro_cliente.model.Cliente;
+import pt.cadastro_cliente.service.ClienteService;
+
+// Controller recebe as requisições HTTP (API REST).
+
 @RestController
 @RequestMapping("/clientes")
 @CrossOrigin(origins = "*")
