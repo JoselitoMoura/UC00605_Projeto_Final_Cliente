c63ec19f20d5918447048c5c356f62e18486a165
JoselitoMoura <joselitodemourabarbosa>
2026-06-13 09:36:47 +0100

    Ajuste no comentário sobre o repository.

--- src/main/java/pt/cadastro_cliente/repository/ClienteRepository.java
+++ src/main/java/pt/cadastro_cliente/repository/ClienteRepository.java
@@ -1,11 +1,10 @@
 package pt.cadastro_cliente.repository;
 
-import pt.cadastro_cliente.model.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-/**
- * Repository é responsável por comunicar com o banco de dados.
- * Aqui não precisamos escrever SQL.
- */
+import pt.cadastro_cliente.model.Cliente;
+
+//Repository é responsável por comunicar com o banco de dados.
+ 
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
 }
